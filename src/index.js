e7ca70f581086b4f8d0d864352b455255a45198d
Andre Wiggins <drewigg@gmail.com>
2019-01-08 01:29:48 -0800

    Remove commented golfing explorations

--- src/index.js
+++ src/index.js
@@ -9,30 +9,17 @@ const channel = new MessageChannel();
 // Flush the callback queue when a message is posted to the message channel
 channel.port1.onmessage = () => {
   // Reset the callback queue to an empty list in case callbacks call
-  // afterFrame. These calls to afterFrame should queue up a new
+  // afterFrame. These nested calls to afterFrame should queue up a new
   // callback to be flushed in the following frame and should not impact the
   // current queue being flushed
-
-  // 193 B
   let toFlush = callbacks;
   callbacks = [];
   let time = performance.now();
   for (let i = 0; i < toFlush.length; i++) {
     // Call all callbacks with the time the flush began, similar to requestAnimationFrame
-    // TODO: Error handling? (https://github.com/facebook/react/pull/14384)
+    // TODO: Error handling?
     toFlush[i](time);
   }
-
-  // 199 B
-  // let time = performance.now();
-  // callbacks.splice(0, callbacks.length).forEach(cb => cb(time));
-
-  // 199 B
-  // let toFlush = callbacks.splice(0, callbacks.length);
-  // let time = performance.now();
-  // for (let i = 0; i < toFlush.length; i++) {
-  //   toFlush[i](time);
-  // }
 };
 
 function postMessage() {
